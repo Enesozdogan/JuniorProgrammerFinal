865176bd1ef590ea7bb47c9bcf9adaf33f30bda3
Enesozdogan <72387932+Enesozdogan@users.noreply.github.com>
2022-09-01 22:51:32 +0300

    LVL1 Craftıng System
    -Added additional Lists contating crafting ingridients

--- Assets/Scripts/Cooking/CookingPot.cs
+++ Assets/Scripts/Cooking/CookingPot.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using TMPro;
 public class CookingPot : MonoBehaviour,IDropHandler
 {
+    public MainManager mainManager;
+    [SerializeField] TextMeshProUGUI meatPotCount,vegePotCount,doughPotCount;
+    
+    public List<GameObject> meat_PotList;
+    public List<GameObject> vege_PotList;
+    public List<GameObject> dough_PotList;
+     public void AddToMeat(GameObject meat){
+        meat_PotList.Add(meat);
+        meatPotCount.text="Meat: "+meat_PotList.Count;
+    }
     public void OnDrop(PointerEventData eventData)
     {
        Debug.Log("Dropped in to : "+gameObject.name );
        if(eventData.pointerDrag!=null){
            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition=GetComponent<RectTransform>().anchoredPosition;
-           if(eventData.pointerDrag.gameObject.CompareTag("Food")){
+           if(eventData.pointerDrag.gameObject.CompareTag("Meat")){
+                AddToMeat(eventData.pointerDrag.gameObject);
+                eventData.pointerDrag.gameObject.SetActive(false);
+           }
+           else if(eventData.pointerDrag.gameObject.CompareTag("Vegetable")){
+
                Destroy(eventData.pointerDrag.gameObject,0.2f);
            }
        }
--- Assets/Scripts/Cooking/MainManager.cs
+++ Assets/Scripts/Cooking/MainManager.cs
@@ -5,8 +5,30 @@ using TMPro;
 public class MainManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI playerName;
+    [SerializeField] TextMeshProUGUI meatCount,vegetableCount,doughCount;
+     
+    public List<GameObject> meatList;
+    public List<GameObject> vegetableList;
+    public List<GameObject> doughList;
 
     private void Start() {
-        playerName.text=DataManager.Instance.userName;
+        if(DataManager.Instance!=null) playerName.text=DataManager.Instance.userName;
+         meatCount.text="Meat: "+meatList.Count;
+         vegetableCount.text="Vegetable: "+vegetableList.Count;
+         doughCount.text="Dough: "+doughList.Count;
+        Debug.Log( "meat: "+ meatList.Count+ " vege: " +vegetableList.Count + " dough: "+ doughList.Count);
+    }
+
+    public void AddToMeat(GameObject meat){
+        meatList.Add(meat);
+        meatCount.text="Meat: "+meatList.Count;
+    }
+    public void AddToVegetable(GameObject vegetable){
+        vegetableList.Add(vegetable);
+        vegetableCount.text="Vegetable: "+vegetableList.Count+1;
+    }
+    public void AddToDough(GameObject dough){
+        doughList.Add(dough);
+        doughCount.text="Dough: "+doughList.Count+1;
     }
 }
